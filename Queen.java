2167eb46ded7e2ade46fd0d1cdf820f79a131b46
Student <Student@fvfyw70zj1wt.hotchkiss.net>
2021-04-01 12:13:11 -0400

    QUeen class finished

--- Queen.java
+++ Queen.java
@@ -2,4 +2,32 @@ public class Queen extends Piece{
     public Queen(boolean white){
         super(white);
     }
+
+    @Override
+    public boolean canMove(Board board, Square start, Square end){
+        
+        if(end.getPiece().isWhite() == this.isWhite()){
+            return false;
+        }
+        
+        int x = Math.abs(start.getPos()[0] - end.getPos()[0]);
+        int y = Math.abs(start.getPos()[1] - end.getPos()[1]);
+
+        //horizontal [2]
+        if(x != 0 && y==0){
+            return board.checkRange(start.getPos()[0], start.getPos()[1], end.getPos()[0], end.getPos()[1], 2);
+        }
+
+        //vertical [1]
+        if(x == 0 && y!=0){
+            return board.checkRange(start.getPos()[0], start.getPos()[1], end.getPos()[0], end.getPos()[1], 1);
+        }
+
+        //diagonal [3]
+        if(x == y && x!=0){
+            return board.checkRange(start.getPos()[0], start.getPos()[1], end.getPos()[0], end.getPos()[1], 3);
+        }
+
+        return false;
+    }
 }
