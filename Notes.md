e4b6b35727a32a9f08859d2d7040f2139b56d9c7
Student <Student@fvfyw70zj1wt.hotchkiss.net>
2021-04-01 13:33:38 -0400

    fixed pieces move logic - including pawn EP and jumpmove logic

--- Notes.md
+++ Notes.md
@@ -1,2 +1,8 @@
 Coding notes:
 -Checking for checks - checkThreat(Square target): store kings loc, and then use a method on board that checks "canMove()" for each opposing piece. use checkThreat in Kings "canMove()" method to check for revealed/induced check
+
+- still need to work on game class, move class, player class
+
+- more rules; checks and revealed checks, promotion, move logic
+
+-when coding moves... for pawns, check if has moved is true; if not true and a jump move, set epvalid true... if has moved true, and epvalid true (from last turn), set epValid false
--- Pawn.java
+++ Pawn.java
@@ -1,5 +1,72 @@
 public class Pawn extends Piece{
+    private boolean hasMoved = false;
+    private boolean ePvalid = false;
+     
+
     public Pawn(boolean white){
         super(white);
+        
+    }
+
+    public boolean ePvalid(){
+        return ePvalid;
+    }
+
+    public void setMoved(boolean moved){
+        hasMoved = moved;
+    }
+
+    public void setEP(boolean valid){
+        ePvalid = valid;
+    }
+
+    @Override
+    public boolean canMove(Board board, Square start, Square end){
+        //check movement, check check taking, check en passant!!!
+        if(end.getPiece().isWhite() == this.isWhite()){
+            return false;
+        }
+
+        int x1 = start.getPos()[0];
+        int y1 = start.getPos()[1];
+        int x2 = end.getPos()[0];
+        int y2 = end.getPos()[1];
+
+        //check straight movement
+        if((x2-x1)==0 && (this.isWhite() == true && (y2-y1)==1 || this.isWhite() == false && (y2-y1)==-1) && board.getSquare(x2,y2).getPiece() == null){
+            return true;
+        }
+
+        //check take
+        if( (Math.abs(x2-x1)) == 1 && (this.isWhite() == true && (y2-y1)==1 || this.isWhite() == false && (y2-y1)==-1) && board.getSquare(x2,y2).getPiece() != null){
+            return true;
+        }
+
+        //check jumpmove
+        if((x2-x1) == 0 && (this.isWhite() == true && (y2-y1)==2 || this.isWhite() == false && (y2-y1)==-2) && this.hasMoved == false && board.getSquare(x2,y2).getPiece() != null){
+            return true;
+        }
+        
+        //somehow check eP?
+        if( (Math.abs(x2-x1)) == 1 && (this.isWhite() == true && (y2-y1)==1 || this.isWhite() == false && (y2-y1)==-1) && board.getSquare(x2,y2).getPiece() == null){
+            if(this.isWhite()){
+                if(board.getSquare(x2,y2-1).getPiece() instanceof Pawn){
+                    if(((Pawn)(board.getSquare(x2,y2-1).getPiece())).ePvalid() && board.getSquare(x2,y2-1).getPiece().isWhite() != this.isWhite()){
+                        return true;
+                    }
+                }
+            }
+            else{
+                if(board.getSquare(x2,y2+1).getPiece() instanceof Pawn){
+                    if(((Pawn)(board.getSquare(x2,y2+1).getPiece())).ePvalid() && board.getSquare(x2,y2+1).getPiece().isWhite() != this.isWhite()){
+                        return true;
+                    }
+                }
+            }
+            
+        }
+
+
+        return false;
     }
 }
