9edd587d8e0e505f8afc1488cef961108964784e
Student <Student@fvfyw70zj1wt.hotchkiss.net>
2021-04-01 17:02:00 -0400

    filled out player + move class

--- Move.java
+++ Move.java
@@ -1,3 +1,27 @@
 public class Move {
+    private Player player;
+    private Square start;
+    private Square end;
+    private Piece moving;
+    private Piece taken;
+    private boolean castleMove = false;
+    private boolean jumpMove = false;
+    private int moveNum;
+
+
+    public Move(Player player, Square start, Square end, Piece moving, Piece taken, boolean castling, boolean jumping, int num){
+        this.player = player;
+        this.start = start;
+        this.end = end;
+        this.moving = moving;
+        this.taken = taken;
+        this.castleMove = castling;
+        this.jumpMove = jumping;
+        this.moveNum = num;
+
+    }
+
     
+
+   
 }
--- Player.java
+++ Player.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class Player {
     
+    private boolean whiteP;
+    private String name;
+    private ArrayList<Piece> pieces = new ArrayList<Piece>();
+    
+    public Player(boolean isWhite, String name){
+        whiteP = isWhite;
+        this.name = name;
+    }
+
+    public boolean playingWhite(){
+        return whiteP;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+
+
 }
+
